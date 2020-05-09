b3b66914479474b08ca77fbad36c5f87279051d5
Artem Kolichenkov <artem@kolichenkov.com>
2020-05-09 20:53:02 +0300

    trying to make python work

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="gigabloat_proto",
+    version="0.1.0",
+    description="Proto file for Gigabloat",
+    author="Artem Kolichenkov",
+    author_email="artem@kolichenkov.com",
+    packages=["gigabloat_proto"],
+    install_requires=[],
+)
