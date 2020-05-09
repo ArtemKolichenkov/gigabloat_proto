b5d9535f2113243677c82405612f381616eac8ea
Artem Kolichenkov <artem@kolichenkov.com>
2020-05-09 20:43:10 +0300

    Added python helper for pulling protofile

---
+++ __init__.py
@@ -0,0 +1,11 @@
+from shutil import copy2
+import pathlib
+import os
+
+
+def copy_protos(destination):
+    if not os.path.isdir(destination):
+        os.makedirs(destination)
+    this_dir = str(pathlib.Path(__file__).parent.absolute())
+    protofile = this_dir + "/gigabloat.proto"
+    copy2(protofile, destination)
